6732e2dcb3587f8dd82f850f66a2848e90f4c352
jinkai <ch_kaijack@outlook.com>
2024-06-02 18:28:04 +0800

    feat: init&add ui components&update astro version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,23 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  root: '.',
-  site: 'https://chkaiblog.eu.org/',
-  output: 'static',
-  integrations: [mdx(), tailwind()],
+	root: ".",
+	site: "https://chkaiblog.eu.org/",
+	output: "static",
+	integrations: [
+		mdx({
+			syntaxHighlight: "shiki",
+			shikiConfig: {
+				themes: {
+					light: "github-light",
+					dark: "github-dark",
+				},
+			},
+		}),
+		tailwind(),
+	],
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				codeBg: 'var(--code-bg)'
+			}
+		},
 	},
-	plugins: [],
+	plugins: [
+		require('@tailwindcss/typography'),
+	],
 }
