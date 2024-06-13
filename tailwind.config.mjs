6b0c20cf1fa0e7673bb9e524f4e81fdcda9adc4b
jinkai <ch_kaijack@outlook.com>
2024-06-13 21:21:32 +0800

    feat: add classify in index&add tailwindcss animation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,6 +8,15 @@ export default {
 				codeTipBg: 'rgb(var(--code-bg) / .8)',
 				link: 'var(--gray-9)',
 				linkHover: 'var(--gray-6)',
+			},
+			animation: {
+				'marquee': 'marquee 10s linear infinite'
+			},
+			keyframes: {
+				'marquee': {
+					'from': { transform: 'translateX(0)' },
+					'to': { transform: 'translateX(-50%)' },
+				}
 			}
 		},
 	},
