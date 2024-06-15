77b0935b6d312dc2f691d7c062716207dfb4345b
jinkai <ch_kaijack@outlook.com>
2024-06-15 21:43:15 +0800

    feat: add search

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 import tailwind from "@astrojs/tailwind";
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,5 +21,6 @@ export default defineConfig({
 			},
 		}),
 		tailwind(),
+		pagefind()
 	],
 });
