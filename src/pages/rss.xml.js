d0372e8205175a2b320fb9f15cd90c938d6add2f
jinkai <ch_kaijack@outlook.com>
2024-06-15 19:34:31 +0800

    feat: add rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,19 @@
 import rss from '@astrojs/rss'
+import { getCollection } from 'astro:content';
 
-export function GET(context) {
+export async function GET(context) {
+  const blog = await getCollection('blogs');
   return rss({
     title: 'Jinmokai’s Blog',
     description: 'A Simple Blog for Documenting Your Life',
     site: context.site,
-    items: [],
+    trailingSlash: false,
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/blogs/${post.slug}/`
+    })),
     customData: `<language>zh-cn</language>`,
   })
 }
