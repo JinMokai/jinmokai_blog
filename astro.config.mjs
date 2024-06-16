0525c001415c8e07a86f2a78f23a9b0b1db28cef
jinkai <ch_kaijack@outlook.com>
2024-06-16 19:38:15 +0800

    feat: Best practices and best SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,37 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import tailwind from "@astrojs/tailwind";
 import pagefind from "astro-pagefind";
+import playformCompress from "@playform/compress";
+import robotsTxt from "astro-robots-txt";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	root: ".",
-	site: "https://chkaiblog.eu.org/",
-	output: "static",
-	prefetch: true,
-	integrations: [
-		mdx({
-			syntaxHighlight: "shiki",
-			shikiConfig: {
-				themes: {
-					light: "github-light",
-					dark: "github-dark",
-				},
-			},
-		}),
-		tailwind(),
-		pagefind()
-	],
+  root: ".",
+  site: "https://chkaiblog.eu.org/",
+  output: "static",
+  prefetch: true,
+  integrations: [mdx({
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      themes: {
+        light: "github-light",
+        dark: "github-dark"
+      }
+    }
+  }), tailwind(), pagefind(), playformCompress({
+    CSS: {
+      lightningcss: {
+        minify: true
+      }
+    },
+    HTML: {
+      "html-minifier-terser": {
+        removeComments: true
+      }
+    },
+    Image: false
+  }), robotsTxt(), sitemap()]
 });
