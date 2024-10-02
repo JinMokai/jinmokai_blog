4322205cda4899f74a206bfffb1c5f4de003a813
JinMokai <59952672+JinMokai@users.noreply.github.com>
2024-10-02 19:40:02 +0800

    fix: astro version 5 route warn fix (#21)

---
+++ src/pages/links/interface/index.ts
@@ -0,0 +1,22 @@
+// 友联
+
+interface link {
+    name: string
+    url: string
+    alt: string
+    title: string
+    desc?: string
+    icon?: string
+}
+
+
+export const links = [
+    {
+        name: 'Heybran',
+        url: 'https://heybran.tech/',
+        alt: 'Heybran',
+        title: 'Heybran',
+        desc: '一位远程开发者，热衷于创建用户友好、易于访问的web和web应用，目前从事远程工作',
+        icon: 'https://heybran.tech/wp-content/uploads/2024/07/cropped-75633537-192x192.jpeg'
+    }
+] as link[]
