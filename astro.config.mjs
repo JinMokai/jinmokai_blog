e50db05c58a817e9849bf425685b41f9ac94fd16
JinKai <59952672+JinMokai@users.noreply.github.com>
2024-10-20 09:31:34 +0800

    feat: update astro version and use lightningcss minify css (#25)
    * feat: update astro version and use lightningcss minify css
    
    * feat: add gizp compress and change renderImage components in astro
    
    * feat: 404 page change
    
    * refactor: seo
    
    * fix: Reduced version to fix development environment access to blog link error

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,8 @@ import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap";
 
 import vue from "@astrojs/vue";
+// compressor must in integrations last
+import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
@@ -24,15 +26,12 @@ export default defineConfig({
       }
     }
   }), tailwind(), pagefind(), playformCompress({
-    // CSS: {
-    //   lightningcss: {
-    //     minify: true
-    //   }
-    //   // csso: {
-    //   //   restructure: false
-    //   // }
-    // },
-    CSS: false,
+    CSS: {
+      lightningcss: {
+        minify: true
+      },
+      csso: false
+    },
     HTML: {
       "html-minifier-terser": {
         removeComments: true
@@ -41,6 +40,9 @@ export default defineConfig({
     Image: false
   }), robotsTxt(), sitemap(), vue({
     jsx: true
+  }), compressor({
+    gzip: true,
+    brotli: false
   })],
   devToolbar: {
     enabled: false
